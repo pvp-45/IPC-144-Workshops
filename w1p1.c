9ecf66f49ec95879b4b8541ffacc331e8e73019a
Pruthvi Patel <96516070+pvp-45@users.noreply.github.com>
2023-01-29 05:07:21 -0500

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+             <assessment name example: Workshop - #1 (Part-1)>
+Full Name : Pruthvi Patel
+Student ID#: 170733216
+Email : ppatel450@myseneca.ca
+Section : IPC 144 NHH
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+******************************************************************************/
+
+#include <stdio.h>
+int main()
+{
+	printf("Workshop 1 Part-1\n");
+	printf("=================\n\n");
+	printf("I'm displaying this using the 'printf' stdio\n");
+	printf("(standard input output) library function!\n\n");
+	printf("Dear teacher,\n\n");
+	printf("  I promise I will work hard from this day onward.\n ");
+	printf(" I acknowledge that practice is extremely important,\n");
+	printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+	printf("Sincerely,\n\n");
+    printf("Pruthvi Patel\n");
+	printf("Student ID# 170733216");
+
+	
+	
+	return 0;
+}
