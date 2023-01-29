5dd5b7ccf25652dc87630ac8e8d0d2076196730f
Pruthvi Patel <96516070+pvp-45@users.noreply.github.com>
2023-01-29 05:31:49 -0500

    Update w3p1.c

--- WS03/w3p1.c
+++ WS03/w3p1.c
@@ -1,6 +1,16 @@
-
-
-
+/*
+*****************************************************************************
+                          Workshop - #2 (P2)
+Full Name  : Pruthvi Patel
+Student ID#: 170733216
+Email      : ppatel450@myseneca.ca
+Section    : NHH
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
 
 #include <stdio.h>
 int main()
